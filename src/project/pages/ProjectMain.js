d8b7da53ce9bb9912aa6076b641430d4951e1ed4
popipopipoi <somt1128@gmail.com>
2023-11-27 14:26:45 +0900

    Feat/#2/ 네비 css 변경

---
+++ src/project/pages/ProjectMain.js
@@ -0,0 +1,12 @@
+import Header from "../../dashBoard/components/common/Header";
+
+function ProjectMain() {
+
+    return (
+        <>
+            <Header/>
+        </>
+    );
+}
+
+export default ProjectMain;
