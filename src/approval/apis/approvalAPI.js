29b8e2c7074efe09e1d921c1c732d8512bf9f005
brorsoo <kimsu010@naver.com>
2023-12-19 16:50:18 +0900

    배포용 소스 추가

--- src/approval/apis/approvalAPI.js
+++ src/approval/apis/approvalAPI.js
@@ -4,6 +4,8 @@ import {getAccessTokenHeader, getRefreshTokenHeader, saveToken} from "../utils/T
 const SERVER_IP = `${process.env.REACT_APP_RESTAPI_SERVER_IP}`;
 const SERVER_PORT = `${process.env.REACT_APP_RESTAPI_SERVER_PORT}`;
 const DEFAULT_URL = `http://${SERVER_IP}:${SERVER_PORT}`;
+// 배포용
+// const DEFAULT_URL = `http://${SERVER_IP}`;
 
 /* 인증이 불필요한 기능을 요청할 때 사용하는 메소드 */
 export const request = async (method, url, headers, data) => {
